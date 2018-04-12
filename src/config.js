cc0c2891d1a088522876ebfaa60b33f00b0c3eec
tony-h-lee <tonyhjlee@gmail.com>
2018-04-12 12:20:32 -0400

    added seed file and changed mongodb name to storypaddle

--- src/config.js
+++ src/config.js
@@ -39,7 +39,7 @@ const config = {
   },
   test: {
     mongo: {
-      uri: 'mongodb://localhost/nobleloot-server-test',
+      uri: 'mongodb://localhost/storypaddle-server-test',
       options: {
         debug: false
       }
@@ -47,7 +47,7 @@ const config = {
   },
   development: {
     mongo: {
-      uri: 'mongodb://localhost/nobleloot-server-dev',
+      uri: 'mongodb://localhost/storypaddle-server-dev',
       options: {
         debug: true
       }
@@ -57,7 +57,7 @@ const config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || 8080,
     mongo: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/nobleloot-server'
+      uri: process.env.MONGODB_URI || 'mongodb://localhost/storypaddle-server'
     }
   }
 }
---
+++ src/seed.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Tony on 2018-04-12.
+ */
